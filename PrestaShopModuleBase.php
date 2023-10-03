014b68b0a1127577125692a9534b949de0a31498
George van Engers <vanengers@gmail.com>
2023-10-03 08:50:40 +0200

    compatible with PrestaShop Validator, at least more.

--- PrestaShopModuleBase.php
+++ PrestaShopModuleBase.php
@@ -1,9 +1,46 @@
 <?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
 
-use Vanengers\PrestashopLibModule\Module\BaseModule;
+use Vanengers\PrestashopLibModule\Module\Abstract\ContainerFunctions;
+use Vanengers\PrestashopLibModule\Module\Abstract\InstallationFunctions;
+use Vanengers\PrestashopLibModule\Module\Abstract\MigrationFunctions;
+use Vanengers\PrestashopLibModule\Module\Abstract\ModuleInplementation;
+use Vanengers\PrestashopLibModule\Module\Hook\ActionDispatcherBefore;
 
-class PrestaShopModuleBase extends BaseModule
+class PrestaShopModuleBase extends Module
 {
+    /** HOOKS */
+    use ActionDispatcherBefore;
+
+    /** Abstract implementations for extendable class (module) */
+    use ModuleInplementation;
+
+    /** Installation functions */
+    use InstallationFunctions;
+
+    /** Container functions */
+    use ContainerFunctions;
+
+    /** Database Migration functions for upgrading */
+    use MigrationFunctions;
+
     public function __construct()
     {
         $this->tab = 'other_modules';
@@ -14,6 +51,15 @@ class PrestaShopModuleBase extends BaseModule
         parent::__construct();
         $this->displayName = $this->l('PrestaShopModuleBase');
         $this->description = $this->l('This is module description');
+
+        $this->autoLoad();
+        try {
+            $this->compile();
+        } catch (Throwable $e) {
+            if (_PS_MODE_DEV_) {
+                dump($e);
+            }
+        }
     }
 
     public function preloadAssignConfigurations(): void
--- sql/install/202308301730 - T - test.sql
+++ sql/install/202308301730 - T - test.sql
@@ -3,4 +3,6 @@
     Format: YYYYMMDDHHMM - Type - TableName.sql
     Example: 202308301730 - I - ps_orders.sql
     Type: I - Insert, U - Update, T - Truncate, D - Delete, A - Alter, C - Create (abbreviation of SQL command)
+    ALLWAYS USE PREFIX FOR TABLES: _DB_PREFIX_ (this will be parsed into the PS prefix)
+    ALTER TABLE `_DB_PREFIX_image` ......  results in ALTER TABLE `ps_image` ......
 */
